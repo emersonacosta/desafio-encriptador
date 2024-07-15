4c676802cc11ee360c64b934bad52f0ce9fcd40b
emersonacosta <emersonacosta@gmail.com>
2024-07-15 17:35:09 -0500

    Update index js y css

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 
 
-function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-    console.log(numeroSecreto);
+function encriptar() {
+    document.getElementById("right").style.display = "none";
 }
-
-function reiniciarJuego() {
-    //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
-    condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
-}
-
-condicionesIniciales();
